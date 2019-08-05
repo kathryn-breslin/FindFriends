7a7489a497fe8e2cb3a4c741c90ca4c48fa9625c
kathryn-breslin <kathryn.l.breslin@gmail.com>
2019-08-05 19:30:27 -0400

    initialize app with files, data, server and routing

---
+++ data/friends.js
@@ -0,0 +1,84 @@
+var friendsList = [
+    {
+        name: 'Antoini Porowski',
+        photo: 'http://www.gstatic.com/tv/thumb/persons/976209/976209_v9_ba.jpg',
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name: 'Tan France',
+        photo: 'https://m.media-amazon.com/images/M/MV5BMTY1MDIzNzg4OF5BMl5BanBnXkFtZTgwMDM3MTA4NDM@._V1_.jpg',
+        scores: [
+            "4",
+            "2",
+            "1",
+            "2",
+            "3",
+            "5",
+            "5",
+            "5",
+            "2",
+            "2"
+        ]
+    }, 
+    {
+        name: 'Karamo Brown', 
+        photo: 'https://static.tvgcdn.net/mediabin/showcards/celebs/k-l/thumbs/karamo-brown-769198_828x1104.png',
+        scores: [
+            "5",
+            "3",
+            "3",
+            "1",
+            "4",
+            "3",
+            "4",
+            "5",
+            "1",
+            "3"
+        ]
+    },
+    {
+        name: 'Bobby Berk', 
+        photo: 'https://m.media-amazon.com/images/M/MV5BNzAwMjg1MTkwNF5BMl5BanBnXkFtZTgwOTI3MTA4NDM@._V1_.jpg',
+        scores: [
+            "1",
+            "2",
+            "3",
+            "5",
+            "4",
+            "2",
+            "1",
+            "1",
+            "5",
+            "2"
+        ]
+    },
+    {
+        name: 'Jonathan Van Ness ', 
+        photo: 'https://s1.r29static.com//bin/entry/ccc/720x864,85/2135981/jonathan-van-ness-is-going-2135981.webp',
+        scores: [
+            "2",
+            "4",
+            "5",
+            "3",
+            "1",
+            "2",
+            "4",
+            "5",
+            "5",
+            "1"
+        ]
+    }
+]
+  
+module.exports = friendsList;
